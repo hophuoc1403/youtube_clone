90eaded69f504e388ef2e06f45e117716cbae8bc
phuocdepzai <97019672+hophuoc1403@users.noreply.github.com>
2022-09-11 10:56:43 +0700

    add link api

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,3 +1,5 @@
+// link  API :  https://rapidapi.com/ytdlfree/api/youtube-v31?utm_source=youtube.com%2FJavaScriptMastery&utm_medium=referral&utm_campaign=DevRel
+
 import axios from "axios";
 
 export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
